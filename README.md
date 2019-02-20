73617906e001837d761fbf971905c74a16e897f4
zemin <zemin@dashebao.com>
2019-02-20 15:02:29 +0800

    no message

---
+++ README.md
@@ -0,0 +1,2 @@
+### hotcss适配移动端方案
+### css-modules 解决命名冲突问题
---
+++ app/js/ious/index.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <div>
+        <Heador/>
+        <Islider/>
+      
+        <Footor/>
+        <Navbar/>
+    </div>
+</template>
+
+<script>
+import Heador from '../public/header.vue'
+import Islider from './islider.vue'
+import Footor from '../public/footer.vue'
+import Navbar from '../public/navbar.vue'
+export default {
+	components:{
+		Heador,
+		Islider,
+		Footor,
+		Navbar,
+	},
+}
+</script>
